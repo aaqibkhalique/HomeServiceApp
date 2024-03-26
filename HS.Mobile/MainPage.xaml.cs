16bddbab0670a128d008541ccacb2c84f860c1ad
Aaqib Khalique <aaqibkhaliq2@gmail.com>
2024-03-26 06:01:34 +0500

    Add project files.

---
+++ HS.Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HS.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HS.Shared/ExampleJsInterop.cs
@@ -0,0 +1,42 @@
+using Microsoft.JSInterop;
+
+namespace HS.Shared
+{
+    // This class provides an example of how JavaScript functionality can be wrapped
+    // in a .NET class for easy consumption. The associated JavaScript module is
+    // loaded on demand when first needed.
+    //
+    // This class can be registered as scoped DI service and then injected into Blazor
+    // components for use.
+
+    public class ExampleJsInterop : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public ExampleJsInterop(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "./_content/HS.Shared/exampleJsInterop.js").AsTask());
+        }
+
+        public async ValueTask<string> Prompt(string message)
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<string>("showPrompt", message);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
+
+
+//FIVERR NOT ALLOW SHARING EMAIL IN CHAT SO I AM SHARING IT HERE 
+
+// aaqibkhaliq2@gmail.com
